d518459504263d995f305734d68c883a7327eedc
Almasgali <ahadyrov@gmail.com>
2024-08-14 17:03:50 +0300

    initial commit

---
+++ backend/src/test/java/com/almasgali/news/NewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almasgali.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
