7ef6d9d0c90f0f650fce5dffa8150632d4cb3057
Оськина Ксения Юрьевна <k.oskina@nicetu.spb.ru>
2024-08-15 15:27:27 +0300

    add authentication form

--- frontend/src/components/AuthenticationForm.vue
+++ frontend/src/components/AuthenticationForm.vue
@@ -1,9 +1,52 @@
 <template>
     <v-form @submit.prevent="">
-        <v-btn :to="{name: 'registration'}">Зарегистрироваться</v-btn>
+        <v-row>
+            <v-col>
+                <h3>Вход в личный аккаунт</h3>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="4">
+                <v-text-field
+                    type="email" 
+                    label="Email"
+                    v-model="person.email" 
+                />
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="4">
+                <v-text-field
+                    type="password" 
+                    label="Пароль"
+                    v-model="person.password" 
+                />
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <v-btn :to="{name: 'home'}">
+                    Войти
+                </v-btn>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <router-link :to="{name: 'registration'}">Нет аккаунта? Создать</router-link>
+            </v-col>
+        </v-row>
     </v-form>
 </template>
 
 <script>
-    
+    export default {
+        data() {
+            return {
+                person: {
+                    email: '',
+                    password: ''
+                }
+            }
+        }
+    }
 </script>
