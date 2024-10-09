b45c747a46979cfc58044474cc9cafe3ccce0eaa
Almasgali <ahadyrov@gmail.com>
2024-10-09 16:40:09 +0300

    controller refactoring

--- backend/src/main/java/com/almasgali/news/controller/ArticleController.java
+++ backend/src/main/java/com/almasgali/news/controller/ArticleController.java
@@ -12,7 +12,6 @@ import com.almasgali.news.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -95,44 +93,5 @@ public class ArticleController {
     @PreAuthorize("hasAuthority('ADMIN')")
     public void deleteComment(@PathVariable long id) {
         articleService.deleteComment(id);
-    }
-
-    @GetMapping("/themes")
-    public List<Theme> getThemes() {
-        return articleService.getThemes();
-    }
-
-    @PostMapping("/themes")
-    @PreAuthorize("hasAuthority('ADMIN')")
-    public void addTheme(@RequestBody ThemeRequest request) {
-        articleService.addTheme(request);
-    }
-
-    @GetMapping("/{id}/themes")
-    public List<Theme> getArticleThemes(@PathVariable long id) {
-        return articleService.getArticleThemes(id);
-    }
-
-    @GetMapping("/themes/filter")
-    public List<Article> filterArticlesByThemes(@RequestBody ArticleThemesRequest request) {
-        return articleService.filterArticlesByThemes(request);
-    }
-
-    @DeleteMapping("/themes/{id}")
-    @PreAuthorize("hasAuthority('ADMIN')")
-    public void deleteTheme(@PathVariable long id) {
-        articleService.deleteTheme(id);
-    }
-
-    @PatchMapping("/{id}/themes")
-    @PreAuthorize("hasAuthority('ADMIN')")
-    public void addThemeToArticle(@PathVariable long id, @RequestParam long themeId) {
-        articleService.addThemeToArticle(id, themeId);
-    }
-
-    @DeleteMapping("/{id}/themes")
-    @PreAuthorize("hasAuthority('ADMIN')")
-    public void deleteThemeFromArticle(@PathVariable long id, @RequestParam long themeId) {
-        articleService.deleteThemeFromArticle(id, themeId);
     }
 }
