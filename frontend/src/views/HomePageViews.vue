dd35d4b2e4ccfde078935a37f8a71563123cd964
Ksenia <ksy.spyy@gmail.com>
2024-10-16 13:29:56 +0300

    fix bug

--- frontend/src/views/HomePageViews.vue
+++ frontend/src/views/HomePageViews.vue
@@ -8,13 +8,11 @@
 <script>
   import AppBar from '../components/AppBar.vue'
   import NewsArticle from '../components/NewsArticle.vue'
-  import AdminPanel from '../components/AdminPanel.vue';
 
   export default {
     components: {
       AppBar,
-      NewsArticle,
-      AdminPanel
+      NewsArticle
     }
   }
 </script>
